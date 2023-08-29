fde47e692f242340942bf96f0f2d1d179304ff3a
Geokaswc <geokolade@gmail.com>
2023-08-29 13:27:38 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * sum_listint - function that returns the sum of all the data 
+ * sum_listint - calculates the sum of all the data in a listint_t list
  * @head: first node in the linked list
  *
- * Return: resulting sum or 0 if the list is empty 
+ * Return: resulting sum
  */
 int sum_listint(listint_t *head)
 {
